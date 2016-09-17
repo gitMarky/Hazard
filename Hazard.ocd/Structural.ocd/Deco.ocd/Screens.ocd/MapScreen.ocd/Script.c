ea6a5e51c1336df71df10f039a8661efd7909cc2
Mark <markh4b@hotmail.com>
2016-09-17 22:20:45 +0200

    Deco: Map screen
    Changed the whole logic for drawing pixels. Performance is bad though!

--- Hazard.ocd/Structural.ocd/Deco.ocd/Screens.ocd/MapScreen.ocd/Script.c
+++ Hazard.ocd/Structural.ocd/Deco.ocd/Screens.ocd/MapScreen.ocd/Script.c
@@ -22,7 +22,7 @@ func Initialized()
 	}
 	// Masterscreen is occupied, initializing this screen.
 	InitScreen();
-	// Only needs the masterscreen
+	// Only needed by the masterscreen
 	map_x = MAP_SCREEN_ZoomFactor / 2;
 	map_y = MAP_SCREEN_ZoomFactor / 2;
 }
@@ -34,54 +34,89 @@ func InitScreen()
 {
 	// Adjusting size.
 	SetShape(-MAP_SCREEN_ScreenWidth / 2, -MAP_SCREEN_ScreenHeight / 2, MAP_SCREEN_ScreenWidth, MAP_SCREEN_ScreenHeight);
+	// Draw black screen.
+	for (var x = 0; x < MAP_SCREEN_ScreenWidth; ++x)
+	for (var y = 0; y < MAP_SCREEN_ScreenHeight; ++y)
+	{
+		InitPixel(x, y);
+		DrawPixel(x, y, RGB(0, 0, 0));
+	}
 	// Adding frame.
 	var frame = AddFrame();
 	frame->PanelColor(RGBa(1, 1, 1, 200));
 	frame->FrameColor(RGBa(255, 255, 255));
 	// Showing the position of the clonks.
 	ScheduleCall(this, this.ShowCrew, 1);
+	ShowLandscape();
 	ScreenLandscape();
+}
+
+func ShowLandscape()
+{
+	// Only the master screen does this
+	if (MAP_SCREEN_MasterScreen != this) return;
+	// Draw the whole landscape
+	for (var x = 0; x < LandscapeWidth(); x += MAP_SCREEN_ZoomFactor)
+	for (var y = 0; y < LandscapeHeight(); y += MAP_SCREEN_ZoomFactor)
+	{
+		DrawLandscape(x, y);
+	}
 }
 
 func ScreenLandscape()
 {
 	// Am I masterscreen?
 	if (MAP_SCREEN_MasterScreen != this) return;
-	// Saving all existing screens.
-	var screens = [];
-	screens = FindObjects(Find_ID(GetID()));
 	// Scanning three pixels of the landscape.
-	for (var j = 0, mat, matC; j < 3; j++)
+	for (var j = 0; j < 3; j++)
 	{
-		mat = GetMaterial(AbsX(map_x), AbsY(map_y));
-		matC = RGB(255, 0, 0); //TODO RGB(GetMaterialColor(mat, 1, 0), GetMaterialColor(mat, 1, 1), GetMaterialColor(mat, 1, 2));
-
-		for (var i = 0; i < GetLength(screens); i++)
-		screens[i]->DrawPixel(
-			(map_x - MAP_SCREEN_ZoomFactor / 2) / MAP_SCREEN_ZoomFactor,
-			(map_y - MAP_SCREEN_ZoomFactor / 2) / MAP_SCREEN_ZoomFactor,
-			matC);
-		// Increasing map_x and map_y.
+		DrawLandscape(map_x, map_y);
+		// Determine next pixel
 		map_x += MAP_SCREEN_ZoomFactor;
-		if ((map_x - MAP_SCREEN_ZoomFactor / 2) / MAP_SCREEN_ZoomFactor > MAP_SCREEN_ScreenWidth)
+		if (map_x > LandscapeWidth())
 		{
-			map_x = MAP_SCREEN_ZoomFactor / 2;
+			map_x = 0;
 			map_y += MAP_SCREEN_ZoomFactor;
-			if ((map_y - MAP_SCREEN_ZoomFactor / 2) / MAP_SCREEN_ZoomFactor > MAP_SCREEN_ScreenHeight)
-				map_y = MAP_SCREEN_ZoomFactor / 2;
+			if (map_y > LandscapeHeight())
+			{
+				map_y = 0;
+			}
 		}
 	}
 	ScheduleCall(this, this.ScreenLandscape, 1, 1);
 }
 
+func DrawLandscape(int global_x, int global_y)
+{
+	var texture = GetTexture(global_x - GetX(), global_y - GetY());
+	var color = GetAverageTextureColor(texture);
+	UpdatePixel(global_x / MAP_SCREEN_ZoomFactor, global_y / MAP_SCREEN_ZoomFactor, color);
+}
+
+func InitPixel(int x, int y)
+{
+	var overlay = GetPixelOverlay(x, y);
+	SetGraphics("Pixel", GetID(), overlay, GFXOV_MODE_Base);
+	SetObjDrawTransform(1000, 0, (x - MAP_SCREEN_ScreenWidth / 2) * 1000, 0, 1000, (y - MAP_SCREEN_ScreenHeight / 2) * 1000, overlay);
+}
+
+func UpdatePixel(int x, int y, int color)
+{
+	var screens = FindObjects(Find_ID(GetID()));
+	for (var screen in screens)
+	{
+		screen->DrawPixel(x, y, color);
+	}
+}
+
 func DrawPixel(int x, int y, int color)
 {
-	// Drawing.
-	// TODO SetLandscapePixel(-MAP_SCREEN_ScreenWidth/2 + x, -MAP_SCREEN_ScreenHeight/2 + y, color);
+	SetClrModulation(color, GetPixelOverlay(x, y));
 }
 
 func ShowCrew()
 {
+	var lifetime = 10;
 	for (map_dot in FindObjects(Find_OCF(OCF_CrewMember))) 
 	{
 		if (map_dot)
@@ -89,9 +124,15 @@ func ShowCrew()
 			// Showing crew member position.
 			var x = map_dot->GetX() / MAP_SCREEN_ZoomFactor;
 			var y = map_dot->GetY() / MAP_SCREEN_ZoomFactor;
-			CreateLEDEffect(map_dot->GetColor(), -MAP_SCREEN_ScreenWidth / 2 + x, -MAP_SCREEN_ScreenHeight / 2 + y, 1, 10);
+			CreateLEDEffect(map_dot->GetColor(), x - MAP_SCREEN_ScreenWidth / 2, y - MAP_SCREEN_ScreenHeight / 2, 2, lifetime * 2);
 		}
 	}
+	ScheduleCall(this, this.ShowCrew, lifetime);
+}
+
+func GetPixelOverlay(int x, int y)
+{
+	return 1 + x + y * MAP_SCREEN_ScreenWidth;
 }
 
 /* Master-functions */
