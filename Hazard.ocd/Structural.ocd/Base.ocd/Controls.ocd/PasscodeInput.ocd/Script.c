23e6df74638e4e2fa3354eac24e8bf0cd773d514
Mark <markh4b@hotmail.com>
2016-10-09 22:16:34 +0200

    Deco: Keypad

---
+++ Hazard.ocd/Structural.ocd/Base.ocd/Controls.ocd/PasscodeInput.ocd/Script.c
@@ -0,0 +1,25 @@
+#include Keypad
+
+func CorrectCodeEffect()
+{
+	CreateLEDEffect(RGB(0, 255, 0), 5, 1, nil, 50);
+}
+  
+func WrongCodeEffect()
+{
+	CreateLEDEffect(RGB(255, 0, 0), 5, 1, nil, 50);
+}
+
+// overloaded
+
+public func OpenDoor(object user)
+{
+	if (target_door) target_door->~Access();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// properties
+
+local Name = "$Name$";
+local Description = "$Description$";
