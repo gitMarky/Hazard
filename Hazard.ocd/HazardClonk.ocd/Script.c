324e6d16d138907edd9dac6c660555625326bee1
Mark <markh4b@hotmail.com>
2016-10-14 18:46:14 +0200

    Experimental: GUI for Hazard Clonk

--- Hazard.ocd/HazardClonk.ocd/Script.c
+++ Hazard.ocd/HazardClonk.ocd/Script.c
@@ -79,3 +79,91 @@ Backflip = {
 
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// GUI
+
+public func HasInteractionMenu(){ return true;}
+
+public func GetInteractionMenus(object crew)
+{
+	var menus = _inherited(crew) ?? [];		
+
+	var priority_base = 50;
+
+	// add menus from contents
+	if (crew == this) // TODO: maybe let friendly clonks access this, too?
+	{
+		for (var item in FindObjects(Find_Container(this)))
+		{
+			if (item && item->~HasInteractionMenu())
+			{
+				var other = item->GetInteractionMenus(crew);
+
+				// shift priority
+				for (var menu in other)
+				{
+					menu.Priority += priority_base;
+					PushBack(menus, menu);
+				}
+			}
+		}
+	}
+
+	return menus;
+}
+
+
+public func GetGUIFiremodeMenuColor(){ return RGB(0, 50, 50);}
+
+
+public func GetGUIFiremodeMenuEntries(object crew)
+{
+	var menu_entries = [];
+
+	// default design of a control menu item
+	var custom_entry = 
+	{
+		Right = "100%", Bottom = "2em",
+		BackgroundColor = {Std = 0, OnHover = 0x50ff0000},
+		image = {Right = "2em"},
+		text = {Left = "2em"}
+	};
+	
+		
+	// Add info message for every defender
+	for (var firemode in this->~GetFiremodes())
+	{
+		var is_available = firemode.condition == nil || Call(firemode.condition);
+		
+		if (!is_available) continue;
+		
+		var firemode_symbol = firemode.icon ?? this;
+
+		PushBack(menu_entries,
+		{
+		    symbol = firemode_symbol,
+		    extra_data = firemode.name,
+			custom = 
+			{
+				Prototype = custom_entry,
+				Priority = GUI_PRIORITY_FIREMODE,
+				text = {Prototype = custom_entry.text, Text = firemode.name},
+				image = {Prototype = custom_entry.image, Symbol = firemode_symbol},
+			}
+		});
+	}
+
+	return menu_entries;
+}
+
+public func OnGUIHoverFiremode(id symbol, string action, desc_menu_target, menu_id)
+{
+	// do nothing at the moment
+}
+
+public func OnGUIChangeFiremode(id symbol, string action, bool alt)
+{
+	// do nothing on click, or maybe let the clonk play an idle animation :p
+}
+
