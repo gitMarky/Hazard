fe43f7c67dbb16d674be87c369e52ec84e0071be
Mark <markh4b@hotmail.com>
2017-03-26 22:15:39 +0200

    Stable: Merge branch 'scen_tutorial' into stable

---
+++ Hazard.ocf/Tutorial.ocs/System.ocg/Constants.c
@@ -0,0 +1,6 @@
+
+static const TUTORIAL_FINISHED_INTRO = "intro";
+static const TUTORIAL_HAS_PISTOL = "pistol";
+static const TUTORIAL_FINISHED = "end";
+
+static const SOUND_ACK = "Ack";
---
+++ Hazard.ocf/Tutorial.ocs/System.ocg/Mod_Trees.c
@@ -0,0 +1,12 @@
+#appendto Library_Plant
+#appendto Library_Tree
+
+func Seed()
+{
+	// do nothing
+}
+
+func DoSeed(bool no_init)
+{
+	// do nothing
+}
