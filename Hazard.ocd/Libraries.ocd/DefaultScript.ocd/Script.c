8ef7129dcce838830b2f79484ec34f9cdd7a79d6
Mark <markh4b@hotmail.com>
2017-04-09 22:28:13 +0200

    Game configuration via scenario parameters
    This is not really finished yet, but it is on a good path. Will probably remove the round manager in the future, since it is not necessary anymore then.

--- Hazard.ocd/Libraries.ocd/DefaultScript.ocd/Script.c
+++ Hazard.ocd/Libraries.ocd/DefaultScript.ocd/Script.c
@@ -1,8 +1,17 @@
 #include Library_ScenarioScript_PlayerRespawn
 
+static const SCENPAR_HAZARD_GOAL_Random = -1;
+static const SCENPAR_HAZARD_GOAL_BombingRun = 1;
+static const SCENPAR_HAZARD_GOAL_CaptureTheFlag = 2;
+static const SCENPAR_HAZARD_GOAL_DeathMatch = 3;
+static const SCENPAR_HAZARD_GOAL_LastManStanding = 4;
+static const SCENPAR_HAZARD_GOAL_Domination = 5;
+static const SCENPAR_HAZARD_GOAL_AlienHunt = 6;
+static const SCENPAR_HAZARD_GOAL_Assault = 7;
+
 local Name = "Default script";
 local Description = "Basic script for scenarios.";
-local Hazard;
+static Hazard;
 
 /**
 	Default script
@@ -14,21 +23,49 @@ local Hazard;
 func Initialize()
 {
 	_inherited(...);
-	Hazard = new Hazard 
-	{
-		Generator = {PowerSupply = false},
-		Radar = {FakeRadar = true}
-	};
+	
+	InitializeProperties();
 
 	CreateObject(Environment_RoundManager);
 	CreateObject(Environment_Configuration);
 }
 
 
-func InitializePlayer(int player)
+private func InitializePlayer(int player)
 {
 	_inherited(player, ...);
 }
+
+private func InitializeProperties()
+{
+	if (nil == Hazard)
+	{
+		Hazard = new Hazard 
+		{
+			Generator = {PowerSupply = false},
+			Radar = {FakeRadar = true},
+			Scenario_Goal = SCENPAR_HAZARD_GOAL_Random,
+		};
+	}
+}
+
+
+/**
+ * Callback from configuration menu object.
+ */
+private func OnConfigurationEnd()
+{
+	_inherited(...);
+
+	// the players have been released from their containers by
+	// the round countdown. Put them in relaunch containers
+	// at the starting position
+	for (var i = 0; i < GetPlayerCount(); i++)
+	{
+		SpawnPlayer(GetPlayerByIndex(i));
+	}
+}
+
 
 /**
  Callback from the round manager.
@@ -47,4 +84,12 @@ private func GetPlayerCrewID(int player)
 private func StartingEquipment(object crew)
 {
 	//nobody gets anything
+}
+
+private func SetGoal(int goal)
+{
+	InitializeProperties();
+	
+	Log("Set goal to %d", goal);
+	Hazard.Scenario_Goal = goal;
 }
