b483cd1f26e73cfd2675b156a915d2414bda0a73
Mark <markh4b@hotmail.com>
2017-04-25 07:00:03 +0200

    Pipeline, part 2

--- Hazard.ocd/Structural.ocd/Base.ocd/Pipe.ocd/Pump.ocd/Script.c
+++ Hazard.ocd/Structural.ocd/Base.ocd/Pipe.ocd/Pump.ocd/Script.c
@@ -6,12 +6,13 @@ local left, right, up, down, valve, connects, eff,con,
       efflux, conflux, temp, temp2,
       running;
 
-public func Initialize() {
-  SetAction("Pump");
-  efflux = CreateArray();
-  conflux = CreateArray();
-  // Standardleistung
-  power = 2;
+func Initialize()
+{
+	SetAction("Pump");
+	efflux = CreateArray();
+	conflux = CreateArray();
+	// Standardleistung
+	power = 2;
 }
 
 /* Wichtig f�r die Pumpen:
@@ -20,149 +21,191 @@ public func Initialize() {
     Zweiter Anschluss -> drain
 */
 
-public func Left(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect) {
-  if(connects >= 2) return();
-
-  return(_inherited(iRepeat, solid, valve, pipeId, iPower, connect));
+func Left(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect)
+{
+	if (connects >= 2)
+		return;
+	return _inherited(iRepeat, solid, valve, pipeId, iPower, connect);
 }
 
-public func Right(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect) {
-  if(connects >= 2) return();
-
-  return(_inherited(iRepeat, solid, valve, pipeId, iPower, connect));
+func Right(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect)
+{
+	if (connects >= 2)
+		return;
+	return _inherited(iRepeat, solid, valve, pipeId, iPower, connect);
 }
 
-public func Up(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect) {
-  if(connects >= 2) return();
-
-  return(_inherited(iRepeat, solid, valve, pipeId, iPower, connect));
+func Up(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect)
+{
+	if (connects >= 2)
+		return;
+	return _inherited(iRepeat, solid, valve, pipeId, iPower, connect);
 }
 
-public func Down(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect) {
-  if(connects >= 2) return();
-
-  return(_inherited(iRepeat, solid, valve, pipeId, iPower, connect));
+func Down(int iRepeat, bool solid, bool valve, id pipeId, int iPower, object connect)
+{
+	if (connects >= 2)
+		return;
+	return _inherited(iRepeat, solid, valve, pipeId, iPower, connect);
 }
 
-public func ConnectTo(object connect, int dir) {
-  if(!connects) con = connect;
-  if(connects == 1) eff = connect;
-  connects++;
-
-  return(_inherited(connect, dir));
+func ConnectTo(object connect, int dir)
+{
+	if (!connects)
+		con = connect;
+	if (connects == 1)
+		eff = connect;
+	connects++;
+	return _inherited(connect, dir);
 }
 
 /* �ffentliches */
 
-public func UpdateEfflux() {
-  efflux = CreateArray();
-
-  if(eff)
-  {
-    if(eff == left)  eff->UpdateSystem(this(), PIPE_Right, 2);
-    if(eff == right) eff->UpdateSystem(this(), PIPE_Left, 2);
-    if(eff == down)  eff->UpdateSystem(this(), PIPE_Up, 2);
-    if(eff == up)    eff->UpdateSystem(this(), PIPE_Down, 2);
-  }
-
-  UpdateConflux();
+func UpdateEfflux()
+{
+	efflux = CreateArray();
+	
+	if (eff)
+	{
+		if (eff == left)
+			eff->UpdateSystem(this, PIPE_Right, 2);
+		if (eff == right)
+			eff->UpdateSystem(this, PIPE_Left, 2);
+		if (eff == down)
+			eff->UpdateSystem(this, PIPE_Up, 2);
+		if (eff == up)
+			eff->UpdateSystem(this, PIPE_Down, 2);
+	}
+	
+	UpdateConflux();
 }
 
-public func UpdateConflux() {
-  conflux = CreateArray();
-
-  if(con)
-  {
-    if(con == left)  con->UpdateSystem(this(), PIPE_Right, 1);
-    if(con == right) con->UpdateSystem(this(), PIPE_Left, 1);
-    if(con == down)  con->UpdateSystem(this(), PIPE_Up, 1);
-    if(con == up)    con->UpdateSystem(this(), PIPE_Down, 1);
-  }
+func UpdateConflux()
+{
+	conflux = CreateArray();
+	
+	if (con)
+	{
+		if (con == left)
+			con->UpdateSystem(this, PIPE_Right, 1);
+		if (con == right)
+			con->UpdateSystem(this, PIPE_Left, 1);
+		if (con == down)
+			con->UpdateSystem(this, PIPE_Up, 1);
+		if (con == up)
+			con->UpdateSystem(this, PIPE_Down, 1);
+	}
 }
 
-public func UpdateSystem(object start, int From, bool pump) {
-  // Andere Pumpe
-  if(pump) start->~TwoPumpError();
-  // Prinzipiell regelt das die Pumpe
-  return();
+func UpdateSystem(object start, int From, bool pump)
+{
+	// Andere Pumpe
+	if (pump)
+		start->~TwoPumpError();
+	// Prinzipiell regelt das die Pumpe
+	return;
 }
 
-public func NewConflux(object new) {
-  conflux[GetLength(conflux)] = new;
+func NewConflux(object new)
+{
+	conflux[GetLength(conflux)] = new;
 }
 
-public func NewEfflux(object new) {
-  efflux[GetLength(efflux)] = new;
+func NewEfflux(object new)
+{
+	efflux[GetLength(efflux)] = new;
 }
 
 // Fehler!
-public func TwoPumpError() {
-  Message("$Error$", this());
+func TwoPumpError()
+{
+	Message("$Error$", this);
 }
 
 /* Pumpen */
 
-protected func Pumping() {
-  if(!running) return();
-  if(efflux[0] == 0) return();
-  if(conflux[0] == 0) return();
-
-  var mat;
-  for(var i=0 ; i < power ; i++)
-    {
-    mat = ExtractLiquid(AbsX(GetX(conflux[temp2])), AbsY(GetY(conflux[temp2])));
-    efflux[temp]->CastLiquid(mat, 35);
-    }
-
-  if(!Random(4)) Smoke(0,0, 15);
-  if(!Random(4)) Bubble(AbsX(GetX(conflux[temp2])), AbsY(GetY(conflux[temp2])));
-
-  temp++;
-  temp2++;
-  if(efflux[temp] == 0) temp = 0;
-  if(conflux[temp2] == 0) temp2 = 0;
+func Pumping()
+{
+	if (!running)
+		return;
+	if (efflux[0] == 0)
+		return;
+	if (conflux[0] == 0)
+		return;
+	
+	var mat;
+	for (var i = 0; i < power; i++)
+	{
+		mat = ExtractLiquid(AbsX(GetX(conflux[temp2])), AbsY(GetY(conflux[temp2])));
+		efflux[temp]->CastLiquid(mat, 35);
+	}
+	
+	if (!Random(4))
+		Smoke(0, 0, 15);
+	if (!Random(4))
+		Bubble(AbsX(GetX(conflux[temp2])), AbsY(GetY(conflux[temp2])));
+	
+	temp++;
+	temp2++;
+	if (efflux[temp] == 0)
+		temp = 0;
+	if (conflux[temp2] == 0)
+		temp2 = 0;
 }
 
-public func On() {
-  running = true;
-  Sound("Gear", 0, this(), 50,0, 1);
+func On()
+{
+	running = true;
+	Sound("Gear", 0, this, 50, 0, 1);
 }
 
-public func Off() {
-  running = false;
-  Sound("Gear", 0, this(), 0,0,-1);
+func Off()
+{
+	running = false;
+	Sound("Gear", 0, this, 0, 0, -1);
 }
 
-public func Status() { return(running); }
+func Status()
+{
+	return running;
+}
 
 /* Steuerkonsole */
 
-public func ConsoleControl(int i) {
-  if(i == 1)
-    {
-    if(running) return("$TurnOff$");
-    else return("$TurnOn$");
-    }
+func ConsoleControl(int i)
+{
+	if (i == 1)
+	{
+		if (running)
+			return "$TurnOff$";
+		else
+			return "$TurnOn$";
+	}
 }
 
-public func ConsoleControlled(int i) {
-  if(i == 1)
-    {
-    if(!running) On();
-    else Off();
-    }
+func ConsoleControlled(int i)
+{
+	if (i == 1)
+	{
+		if (!running)
+			On();
+		else
+			Off();
+	}
 }
 
 /* Steuerung mit Schalter */
 func Access()
 {
-  if(running) Off();
-  else On();
+	if (running)
+		Off();
+	else
+		On();
 }
 
 /* Serialisierung */
 
-public func Serialize(array& extra)
+func Serialize(array extra)
 {
 	_inherited(extra);
 	if (running)
--- Hazard.ocd/Structural.ocd/Base.ocd/Pipe.ocd/Valve.ocd/Script.c
+++ Hazard.ocd/Structural.ocd/Base.ocd/Pipe.ocd/Valve.ocd/Script.c
@@ -3,21 +3,26 @@
 
 local open;
 
-protected func Initialize() {
-  open = false;
-  SetAction("Exist");
+func Initialize()
+{
+	open = false;
+	SetAction("Exist");
 }
 
-protected func ControlDig() {
-  open = !open;
-  UpdatePipesystem();
+func ControlDig()
+{
+	open = !open;
+	UpdatePipesystem();
 }
 
-public func Status() { return(open); }
+func Status()
+{
+	return open;
+}
 
 /* Serialisierung */
 
-public func Serialize(array& extra)
+func Serialize(array extra)
 {
 	if (open)
 		extra[GetLength(extra)] = "ControlDig()";
