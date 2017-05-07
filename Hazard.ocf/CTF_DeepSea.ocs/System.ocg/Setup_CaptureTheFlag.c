fb11b947f940c71cd50ff5376135cfc8e3f8035d
Mark <markh4b@hotmail.com>
2017-05-07 15:12:22 +0200

    Enable CTF goal

---
+++ Hazard.ocf/CTF_DeepSea.ocs/System.ocg/Setup_CaptureTheFlag.c
@@ -0,0 +1,10 @@
+
+#appendto Goal_CaptureTheFlagEx
+
+public func DoSetup(int round)
+{
+	SetFlagBase(1,  285,  140);
+	SetFlagBase(2, 2435, 1130);
+	SetFlagBase(3,  120,  970);
+	SetFlagBase(4, 2275,  700);
+}
