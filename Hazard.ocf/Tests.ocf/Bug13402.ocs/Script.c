e651cc0d970e5cfa0c53b9e6fe1c90312b3b1cae
Mark <markh4b@hotmail.com>
2017-05-24 21:07:39 +0200

    Fix Hazard bug 13402
    The weapon did not stop using when the user dropped the weapon.

--- Hazard.ocf/Tests.ocf/Bug13402.ocs/Script.c
+++ Hazard.ocf/Tests.ocf/Bug13402.ocs/Script.c
@@ -23,10 +23,10 @@ global func Shoot(object weapon, object bot)
 	weapon->ControlUseAltHolding(bot, +100, -100); // fire 45° angle upwards
 }
 
-/*-- Tests --*/
+/*-- Test helpers --*/
 
 // override for the weapon: kills its user
-global func Test1_OnRecovery(object user, proplist firemode)
+global func Bug13402_OnRecoveryDie(object user, proplist firemode)
 {
 	Log("On Recovery - kill the weapon user");
 	_inherited(user, firemode);
@@ -38,7 +38,19 @@ global func Test1_OnRecovery(object user, proplist firemode)
 }
 
 
-global func Test1_Departure(object container)
+global func Bug13402_OnRecoveryDrop(object user, proplist firemode)
+{
+	Log("On Recovery - drop the weapon");
+	_inherited(user, firemode);
+	
+	if (user)
+	{
+		this->Exit();
+	}
+}
+
+
+global func Bug13402_Departure(object container)
 {
 	Log("Departure - print all effects that are active in the weapon");
 	
@@ -51,26 +63,41 @@ global func Test1_Departure(object container)
 	_inherited(container);
 }
 
-global func Test1_OnStartCooldown(object user, proplist firemode)
+global func Bug13402_OnStartCooldown(object user, proplist firemode)
 {
 	Log("On start cooldown - weapon has cooldown, should finish the sound");
+	Sound("Weapon::Minigun::MiniGun", nil, nil, nil, -1);
 	_inherited(user, firemode);
+}
 
+global func Bug13402_OnFinishCooldown(object user, proplist firemode)
+{
+	Log("On finish cooldown - weapon has correct cooldown, did finish the sound");
+	Sound("Weapon::Minigun::MiniTurn", nil, nil, nil, -1);
+	_inherited(user, firemode);
+	
 	if (!CurrentTest().Weapon->Contained())
 	{
 		PassTest();
-	}		
+	}
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Tests
 
 global func Test1_OnStart(int player)
 {
+	Log("-- Test 1: Bug 13402 - Weapon should stop infinite sound when user dies --");
+
 	CurrentTest().Bot = CreateObject(HazardClonk, 80, 190, NO_OWNER);
 
 	CurrentTest().Weapon = CurrentTest().Bot->CreateContents(Weapon_Minigun);
 	CurrentTest().Weapon.GetAmmoSource = Global.GetInfiniteAmmoSource;
-	CurrentTest().Weapon.OnRecovery = Global.Test1_OnRecovery; // Override the recovery function
-	CurrentTest().Weapon.Departure = Global.Test1_Departure;
-	CurrentTest().Weapon.OnStartCooldown = Global.Test1_OnStartCooldown; // Override the cooldown, so that the test passes if the weapon cools down
+	CurrentTest().Weapon.OnRecovery = Global.Bug13402_OnRecoveryDie; // Override the recovery function
+	CurrentTest().Weapon.Departure = Global.Bug13402_Departure;
+	CurrentTest().Weapon.OnStartCooldown = Global.Bug13402_OnStartCooldown; // Override the cooldown, so that the test passes if the weapon cools down
+	CurrentTest().Weapon.OnFinishCooldown = Global.Bug13402_OnFinishCooldown; // Override the cooldown, so that the test passes if the weapon cools down
 
 	return true;
 }
@@ -101,5 +128,51 @@ global func Test1_OnFinished()
 	CurrentTest().Weapon->RemoveObject();
 	CurrentTest().Bot->RemoveObject();
 	CurrentTest().BotDied = false;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+global func Test2_OnStart(int player)
+{
+	Log("-- Test 2: Bug 13402 - Weapon should stop infinite sound when the user drops it --");
+
+	CurrentTest().Bot = CreateObject(HazardClonk, 80, 190, NO_OWNER);
+
+	CurrentTest().Weapon = CurrentTest().Bot->CreateContents(Weapon_Minigun);
+	CurrentTest().Weapon.GetAmmoSource = Global.GetInfiniteAmmoSource;
+	CurrentTest().Weapon.OnRecovery = Global.Bug13402_OnRecoveryDrop; // Override the recovery function
+	CurrentTest().Weapon.Departure = Global.Bug13402_Departure;
+	CurrentTest().Weapon.OnStartCooldown = Global.Bug13402_OnStartCooldown; // Override the cooldown, so that the test passes if the weapon cools down
+	CurrentTest().Weapon.OnFinishCooldown = Global.Bug13402_OnFinishCooldown; // Override the cooldown, so that the test passes if the weapon cools down
+
+	return true;
+}
+
+global func Test2_Execute()
+{
+	if (CurrentTest().Bot->Contents())
+	{
+		Shoot(CurrentTest().Weapon, CurrentTest().Bot);
+		return Wait();	
+	}
+	else
+	{
+		if (CurrentTest().BotDroppedWeapon)
+		{
+			return true;
+		}
+		else
+		{
+			CurrentTest().BotDroppedWeapon = true;
+			return Wait(120);
+		}
+	}
+}
+
+global func Test2_OnFinished()
+{
+	CurrentTest().Weapon->RemoveObject();
+	CurrentTest().Bot->RemoveObject();
+	CurrentTest().BotDroppedWeapon = false;
 }
 
